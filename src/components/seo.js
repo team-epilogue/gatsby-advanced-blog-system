788c23de2cba45e0ca6e7f82a640f45d84c62e30
yunaghgh <yunaghgh@naver.com>
2022-11-15 13:01:07 +0900

    fix: graghql 오류 수정

--- src/components/seo.js
+++ src/components/seo.js
@@ -18,7 +18,9 @@ const SEO = ({ description, lang, meta, keywords, title }) => {
           siteMetadata {
             title
             description
-            author
+            author {
+              name
+            }
           }
         }
       }
@@ -57,7 +59,7 @@ const SEO = ({ description, lang, meta, keywords, title }) => {
         },
         {
           name: `twitter:creator`,
-          content: site.siteMetadata.author,
+          content: site.siteMetadata.author.name,
         },
         {
           name: `twitter:title`,
